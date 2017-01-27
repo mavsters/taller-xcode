c2692063c2f8f2a985da4058cbda9a3bf7c67ec5
Luis <luisrollongordo@gmail.com>
2017-01-27 16:47:44 +0100

    Initial Commit

---
+++ CollectionView_Intro_Inicio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView_Intro_Inicio
+//
+//  Created by Luis Rollon Gordo on 27/1/17.
+//  Copyright © 2017 EfectoApple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
