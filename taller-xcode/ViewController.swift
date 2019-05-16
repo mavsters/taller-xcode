5bc7eeed846da02833cb29779300cf126e4c7955
CSalaC5 <csalac5@imac-de-csalac5.local>
2019-05-16 09:30:49 -0500

    Initial Commit

---
+++ taller-xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taller-xcode
+//
+//  Created by CSalaC5 on 16/05/19.
+//  Copyright © 2019 Supermavster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
